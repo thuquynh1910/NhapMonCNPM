6584ce5521c1f3d8eea7b49912874c1434ad5df2
Trần Anh Tú <80766719+thuquynh1910@users.noreply.github.com>
2022-05-06 14:20:10 +0700

    Create fake_thu_thu.sql

---
+++ Query/fake_thu_thu.sql
@@ -0,0 +1,23 @@
+﻿-- Bảng thủ thư
+-- Chứ tài khoản, mật khẩu
+/* CREATE TABLE THUTHU 
+(
+	TaiKhoan char(30)primary key,
+	MatKhau char(30),
+	HoVaTen char(40),
+	NgaySinh datetime,
+	DiaChi char(100),
+) */
+
+-- Hiển thị top 100 tài khoản
+SELECT TOP (100) [TaiKhoan]
+      ,[MatKhau], [HoVaTen], [NgaySinh],[DiaChi]
+  FROM [QUANLYSACH].[dbo].[THUTHU]
+
+-- Tại fake tài khoản
+INSERT INTO QUANLYSACH.dbo.THUTHU (TaiKhoan, MatKhau, HoVaTen, NgaySinh, DiaChi)
+VALUES ('admin@gmail.com', 'Admin123', N'Trần Thu Quỳnh', '2002-10-9', N'Tân Kiều, Ninh Đa, Ninh Hòa, Khánh Hòa');
+
+-- Check đăng nhập
+SELECT * FROM THUTHU WHERE TaiKhoan='admin@gmail.com' AND MatKhau='Admin123'
+-- SELECT FROM THUTHU WHERE TaiKhoan='" + email + "' AND MatKhau='" + password + "'
