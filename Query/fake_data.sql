59257f92b78937cc1c0c722efedc91625eea5d43
Trần Anh Tú <80766719+thuquynh1910@users.noreply.github.com>
2022-05-22 14:53:50 +0700

    Create fake_data.sql

---
+++ Query/fake_data.sql
@@ -0,0 +1,26 @@
+﻿SELECT * FROM SACH
+
+INSERT INTO QUANLYSACH.dbo.SACH 
+(TenSach, Matheloai, TacGia, NhaXuatBan, NamXuatBan, NgayNhap, KhoangCachXuatBan, MaTinhTrang)
+OUTPUT INSERTED.MaSach
+VALUES (N'Chiến Binh Cầu Vồng', 'TL0002', N'Andrea Hirata',N'Hội Nhà Văn' ,2020, '2022-5-21', 5, 'TT0003');
+
+INSERT INTO QUANLYSACH.dbo.SACH 
+(TenSach, Matheloai, TacGia, NhaXuatBan, NamXuatBan, NgayNhap, KhoangCachXuatBan, MaTinhTrang)
+OUTPUT INSERTED.MaSach
+VALUES (N'Bắt Trẻ Đồng Xanh', 'TL0002', N'Jerome David Salinger',N'Hội Nhà Văn' ,2020, '2022-5-21', 2, 'TT0003');
+
+INSERT INTO QUANLYSACH.dbo.SACH 
+(TenSach, Matheloai, TacGia, NhaXuatBan, NamXuatBan, NgayNhap, KhoangCachXuatBan, MaTinhTrang)
+OUTPUT INSERTED.MaSach
+VALUES (N'ĐÀM ĐẠO VỚI LÃO TỬ ', 'TL0002', N'Lưu Ngôn', N'Văn Học' ,2021, '2022-5-21', 1, 'TT0003');
+
+INSERT INTO QUANLYSACH.dbo.SACH 
+(TenSach, Matheloai, TacGia, NhaXuatBan, NamXuatBan, NgayNhap, KhoangCachXuatBan, MaTinhTrang)
+OUTPUT INSERTED.MaSach
+VALUES (N'Ly ca', 'TL0003', N'Đỗ Doãn Phương', N'Hội Nhà Văn' ,2021, '2022-5-21', 1, 'TT0003');
+
+INSERT INTO QUANLYSACH.dbo.SACH 
+(TenSach, Matheloai, TacGia, NhaXuatBan, NamXuatBan, NgayNhap, KhoangCachXuatBan, MaTinhTrang)
+OUTPUT INSERTED.MaSach
+VALUES (N'Thất Lạc Cõi Người ', 'TL0002', N'Dazai Osamu', N'Văn Học' ,2022, '2022-5-21', 0, 'TT0003');
